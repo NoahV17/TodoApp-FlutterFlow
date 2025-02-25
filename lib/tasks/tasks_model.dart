60ea071118bb886ba583b04af3cdf286e311fc34
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-02-25 01:30:47 +0000

    Updating to latest FlutterFlow output.

--- lib/tasks/tasks_model.dart
+++ lib/tasks/tasks_model.dart
@@ -1,8 +1,19 @@
+import '/backend/api_requests/api_calls.dart';
 import '/flutter_flow/flutter_flow_util.dart';
+import '/index.dart';
 import 'tasks_widget.dart' show TasksWidget;
 import 'package:flutter/material.dart';
 
 class TasksModel extends FlutterFlowModel<TasksWidget> {
+  ///  Local state fields for this page.
+
+  String zenQuote = ' ';
+
+  ///  State fields for stateful widgets in this page.
+
+  // Stores action output result for [Backend Call - API (Zen call)] action in tasks widget.
+  ApiCallResponse? quoteCall;
+
   @override
   void initState(BuildContext context) {}
 
